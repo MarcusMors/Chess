73fe1f3d266c5724a1cd1d69125fc0aa2757e41e
Marcus Mors <alivezeh@gmail.com>
2020-10-30 17:27:40 -0500

    😎 Initial Commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+//data structures
+#include <vector>
+#include <tuple>
+#include <array>
+#include <stack>
+
+//mors army
+#include <cstdlib> //program control, sort, rand nums, search & dynamic memory allocation
+#include <string>
+#include <stdlib.h> //srand, rand
+#include <cmath>
+
+//c++20
+#include <format>  //format([object Object], )
+#include <compare> //(a <=> b) returns an obj susch that < 0 || == 0 || > 0
+
+using namespace std;
+
+int main()
+{
+
+	return 0;
+}
