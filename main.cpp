8e4311b8542a3d57e3c8038a0225da6786bacec9
Mark Romero <geraldo.romero@ucsp.edu.pe>
2020-11-02 23:24:23 -0500

    Una a mas

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-//gAAAAAAAAAAAA TIAA GAAAAAAAAAAAA
+//gAAAAAAAAAAAA TIAA GAAAAAAAAAAAA come pelotas
+
 void swap(char tabla[8][8], int x, int y, int a, int b)
 {
 	int temp = tabla[x][y];
