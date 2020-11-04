66906d45d190fca46afd43596c7eefd39193482a
Mark Romero <geraldo.romero@ucsp.edu.pe>
2020-11-04 13:11:05 -0500

    Ordenando lo que Jose desordeno

--- main.cpp
+++ main.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 using namespace std;
-//gAAAAAAAAAAAA TIAA GAAAAAAAAAAAA come pelotas
-
 void swap(char tabla[8][8], int x, int y, int a, int b){
         int temp = tabla[x][y];
         tabla[x][y] = tabla[a][b];
