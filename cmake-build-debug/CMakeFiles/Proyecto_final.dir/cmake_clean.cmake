7bb8c3774ab50e6816ce4551c66d5bcfa8660039
Gabriel <gabriel.choque@ucsp.edu.pe>
2020-11-03 22:58:29 -0600

    Inicio del proyecto final

---
+++ cmake-build-debug/CMakeFiles/Proyecto_final.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Proyecto_final.dir/main.cpp.obj"
+  "Proyecto_final.exe"
+  "Proyecto_final.exe.manifest"
+  "Proyecto_final.pdb"
+  "libProyecto_final.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Proyecto_final.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
